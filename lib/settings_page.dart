c332f796b15e07a732da028ecd9411e13eefdecf
Alex Zani <alex@zfc.io>
2025-06-17 00:44:45 -0700

    Wrap setting groups in their own widgets to simplify organization.

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -14,47 +14,89 @@ class SettingsPage extends StatelessWidget {
     print('building settings page');
     return ListView(
       children: [
-        const ListTile(title: Text('CW')),
+        CWSettings(appState: appState),
         const Divider(),
-        NumSettingChevron(
-          label: "WPM",
-          initialValue: appState.appConfig.cw.wpm,
-          min: 5,
-          max: 40,
-          step: 1,
-          onSelected: (int i) {
-            appState.appConfig.cw.wpm = i;
+        TTSSettings(appState: appState),
+        const Divider(),
+        FarnsworthSettings(appState: appState),
+        BoolSetting(
+          label: "Force Latest Letter",
+          initialValue: appState.appConfig.farnsworth.forceLatest,
+          onChanged: (bool v) {
+            appState.appConfig.farnsworth.forceLatest = v;
           },
         ),
-        NumSettingChevron(
-          label: "EWPM",
-          initialValue: appState.appConfig.cw.ewpm,
-          min: 5,
-          max: 40,
-          step: 1,
-          onSelected: (int i) {
-            appState.appConfig.cw.ewpm = i;
-          },
-        ),
-        NumSettingChevron(
-          label: "Frequency",
-          initialValue: appState.appConfig.cw.frequency,
-          min: 400,
-          max: 1000,
-          step: 50,
-          onSelected: (int i) {
-            appState.appConfig.cw.frequency = i;
-          },
-        ),
+      ],
+    );
+  }
+}
+
+class FarnsworthSettings extends StatelessWidget {
+  const FarnsworthSettings({
+    super.key,
+    required this.appState,
+  });
+
+  final MyAppState appState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const ListTile(title: Text('Farnsworth')),
+        const Divider(),
         ListSetting(
-          label: "Sample Rate",
-          initialValue: appState.appConfig.cw.sampleRate,
-          values: const [44100, 22050, 11025],
-          onSelected: (int i) {
-            appState.appConfig.cw.sampleRate = i;
+          label: "Level",
+          initialValue: appState.appConfig.farnsworth.level,
+          values: appState.appConfig.farnsworth.letters.split(''),
+          onSelected: (String i) {
+            appState.appConfig.farnsworth.level = i;
           },
         ),
-        const Divider(),
+        NumSettingChevron(
+          label: "Letters Per Group",
+          initialValue: appState.appConfig.farnsworth.groupSize,
+          min: 1,
+          max: 10,
+          step: 1,
+          onSelected: (int i) {
+            appState.appConfig.farnsworth.groupSize = i;
+          },
+        ),
+        NumSettingChevron(
+          label: "Number of Groups",
+          initialValue: appState.appConfig.farnsworth.groupNum,
+          min: 1,
+          max: 15,
+          step: 1,
+          onSelected: (int i) {
+            appState.appConfig.farnsworth.groupNum = i;
+          },
+        ),
+        BoolSetting(
+          label: "Repeat",
+          initialValue: appState.appConfig.farnsworth.repeat,
+          onChanged: (bool v) {
+            appState.appConfig.farnsworth.repeat = v;
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class TTSSettings extends StatelessWidget {
+  const TTSSettings({
+    super.key,
+    required this.appState,
+  });
+
+  final MyAppState appState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
         const ListTile(title: Text('Text-to-Speech')),
         const Divider(),
         NumSettingChevron(
@@ -97,49 +139,61 @@ class SettingsPage extends StatelessWidget {
             appState.appConfig.farnsworth.delay = i;
           },
         ),
+      ],
+    );
+  }
+}
+
+class CWSettings extends StatelessWidget {
+  const CWSettings({
+    super.key,
+    required this.appState,
+  });
+
+  final MyAppState appState;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const ListTile(title: Text('CW')),
         const Divider(),
-        const ListTile(title: Text('Farnsworth')),
-        const Divider(),
+        NumSettingChevron(
+          label: "WPM",
+          initialValue: appState.appConfig.cw.wpm,
+          min: 5,
+          max: 40,
+          step: 1,
+          onSelected: (int i) {
+            appState.appConfig.cw.wpm = i;
+          },
+        ),
+        NumSettingChevron(
+          label: "EWPM",
+          initialValue: appState.appConfig.cw.ewpm,
+          min: 5,
+          max: 40,
+          step: 1,
+          onSelected: (int i) {
+            appState.appConfig.cw.ewpm = i;
+          },
+        ),
+        NumSettingChevron(
+          label: "Frequency",
+          initialValue: appState.appConfig.cw.frequency,
+          min: 400,
+          max: 1000,
+          step: 50,
+          onSelected: (int i) {
+            appState.appConfig.cw.frequency = i;
+          },
+        ),
         ListSetting(
-          label: "Level",
-          initialValue: appState.appConfig.farnsworth.level,
-          values: appState.appConfig.farnsworth.letters.split(''),
-          onSelected: (String i) {
-            appState.appConfig.farnsworth.level = i;
-          },
-        ),
-        NumSettingChevron(
-          label: "Letters Per Group",
-          initialValue: appState.appConfig.farnsworth.groupSize,
-          min: 1,
-          max: 10,
-          step: 1,
+          label: "Sample Rate",
+          initialValue: appState.appConfig.cw.sampleRate,
+          values: const [44100, 22050, 11025],
           onSelected: (int i) {
-            appState.appConfig.farnsworth.groupSize = i;
-          },
-        ),
-        NumSettingChevron(
-          label: "Number of Groups",
-          initialValue: appState.appConfig.farnsworth.groupNum,
-          min: 1,
-          max: 15,
-          step: 1,
-          onSelected: (int i) {
-            appState.appConfig.farnsworth.groupNum = i;
-          },
-        ),
-        BoolSetting(
-          label: "Repeat",
-          initialValue: appState.appConfig.farnsworth.repeat,
-          onChanged: (bool v) {
-            appState.appConfig.farnsworth.repeat = v;
-          },
-        ),
-        BoolSetting(
-          label: "Force Latest Letter",
-          initialValue: appState.appConfig.farnsworth.forceLatest,
-          onChanged: (bool v) {
-            appState.appConfig.farnsworth.forceLatest = v;
+            appState.appConfig.cw.sampleRate = i;
           },
         ),
       ],
